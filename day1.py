e4f65faf9227a02cd42c65bef734173ed81c38fc
Nowshad Hossain Rahat <nowshad.hossain.rahat@gmail.com>
2024-07-05 07:11:12 +0600

    day2

--- day1.py
+++ day1.py
@@ -1,5 +1,3 @@
-import numpy as np
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-numbers = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-
-print(numbers)
+print(list1)
---
+++ day2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+heights = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+weights = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+
+bmi = weights * heights
+
+print(bmi)
